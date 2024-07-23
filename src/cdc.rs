52c2fbcb871f33b167882e011fbc9e3ce914eeb6
danielhe4rt <hey@danielheart.dev>
2024-07-23 06:31:24 -0300

    feat: setup cdc workers

---
+++ src/cdc.rs
@@ -0,0 +1,135 @@
+use std::ops::Add;
+use std::sync::Arc;
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::time::SystemTime;
+use anyhow::Error;
+use async_trait::async_trait;
+use charybdis::operations::{Find, Insert};
+use charybdis::types::Uuid;
+use chrono::{DateTime, Utc};
+use log::debug;
+use scylla::{CachingSession, Session, SessionBuilder};
+use scylla::frame::value::CqlTimeuuid;
+use scylla_cdc::consumer::{CDCRow, Consumer, ConsumerFactory};
+use scylla_cdc::log_reader::CDCLogReaderBuilder;
+
+use crate::models::order::OrderQueue;
+use crate::models::ticket::Ticket;
+
+struct OrderQueueConsumer {
+    session: Arc<CachingSession>,
+}
+
+#[async_trait]
+impl Consumer for OrderQueueConsumer {
+    async fn consume_cdc(&mut self, mut data: CDCRow<'_>) -> anyhow::Result<()> {
+        let (secs, nanos) = data.time.get_timestamp().unwrap().to_unix();
+        let cdc_timestamp = DateTime::from_timestamp(secs as i64, nanos).unwrap();
+
+        let ttl = 5;
+
+        let is_already_processed = cdc_timestamp.add(chrono::Duration::seconds(ttl)) < Utc::now();
+
+        if !is_already_processed {
+            return Ok(());
+        }
+
+        let mut ticket = Ticket {
+            ticket_id: data.take_value("ticket_id").unwrap().as_uuid().unwrap(),
+            event_id: data.take_value("event_id").unwrap().as_uuid().unwrap(),
+            ..Default::default()
+        };
+
+        let processing_ticket = ticket
+            .maybe_find_by_primary_key()
+            .execute(&self.session)
+            .await?;
+
+        if processing_ticket.is_none() {
+            delete_cdc_row(&self.session, data).await?;
+            return Ok(());
+        }
+
+        let mut processing_ticket = processing_ticket.unwrap();
+        if processing_ticket.status != "processing" {
+            delete_cdc_row(&self.session, data).await?;
+            return Ok(());
+        }
+
+        debug!("Purchase canceled due time limit: {:?}", ticket.ticket_id);
+
+        ticket.status = "available".to_string();
+        ticket.insert().execute(&self.session).await?;
+
+        delete_cdc_row(&self.session, data).await?;
+
+        Ok(())
+    }
+}
+
+async fn delete_cdc_row(session: &Arc<CachingSession>, data: CDCRow<'_>) -> Result<(), Error> {
+    session.execute("DELETE FROM order_queue_scylla_cdc_log WHERE \"cdc$stream_id\" = ? AND \"cdc$time\" = ? AND \"cdc$batch_seq_no\" = ?", (
+        data.stream_id,
+        CqlTimeuuid::from(data.time),
+        data.batch_seq_no,
+    )).await?;
+    Ok(())
+}
+struct OrderQueueConsumerFactory {
+    session: Arc<CachingSession>,
+}
+
+#[async_trait]
+impl ConsumerFactory for OrderQueueConsumerFactory {
+    async fn new_consumer(&self) -> Box<dyn Consumer> {
+        Box::new(OrderQueueConsumer {
+            session: self.session.clone(),
+        })
+    }
+}
+
+struct CountingConsumer {
+    counter: Arc<AtomicUsize>,
+}
+
+#[async_trait]
+impl Consumer for CountingConsumer {
+    async fn consume_cdc(&mut self, _: CDCRow<'_>) -> anyhow::Result<()> {
+        let curr = self.counter.fetch_add(1, Ordering::SeqCst);
+        println!("Row no.{}", curr + 1);
+        Ok(())
+    }
+}
+
+
+pub async fn start_cdc_worker(regular_session: Arc<Session>, consumer_session: Arc<CachingSession>) -> anyhow::Result<()> {
+    let factory = Arc::new(OrderQueueConsumerFactory {
+        session: consumer_session.clone(),
+    });
+
+    loop {
+        let end = chrono::Duration::from_std(
+            SystemTime::now()
+                .duration_since(SystemTime::UNIX_EPOCH)
+                .unwrap(),
+        ).unwrap();
+        let start = end - chrono::Duration::seconds(30);
+
+        let (_, handle) = CDCLogReaderBuilder::new()
+            .session(regular_session.clone())
+            .keyspace("ks")
+            .table_name("order_queue")
+            .start_timestamp(start)
+            .end_timestamp(end)
+            .safety_interval(chrono::Duration::seconds(1).to_std().unwrap())
+            .sleep_interval(chrono::Duration::seconds(1).to_std().unwrap())
+            .consumer_factory(factory.clone())
+            .build()
+            .await
+            .expect("Creating the log reader failed!");
+
+        handle.await?;
+    }
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,50 @@
-fn main() {
-    println!("Hello, world!");
+use std::sync::Arc;
+use pretty_env_logger::env_logger::{Builder, Env};
+use scylla::{CachingSession, SessionBuilder};
+use tokio::task::JoinSet;
+use log::debug;
+
+mod models;
+mod cdc;
+mod worker;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::fmt::init();
+
+    //Builder::from_env(Env::default().default_filter_or("info")).init();
+    debug!("Starting up");
+
+    let regular_session = Arc::new(SessionBuilder::new()
+        .known_node("127.0.0.1:9042")
+        .build()
+        .await?
+    );
+
+    let consumer_session = SessionBuilder::new()
+        .known_node("127.0.0.1:9042")
+        .build()
+        .await?;
+
+    consumer_session.use_keyspace("ks", true).await?;
+    let cdc_session = Arc::new(CachingSession::from(consumer_session, 20));
+    let worker_session = Arc::clone(&cdc_session);
+
+    let mut set = JoinSet::new();
+
+    set.spawn(async move {
+        cdc::start_cdc_worker(regular_session, Arc::clone(&cdc_session)).await.unwrap();
+    });
+
+    set.spawn(async move {
+        let db = Arc::clone(&worker_session);
+        worker::handle_events(Arc::clone(&db)).await.unwrap();
+    });
+
+    while let Some(res) = set.join_next().await {
+        let out = res?;
+        // ...
+    }
+
+    Ok(())
 }
